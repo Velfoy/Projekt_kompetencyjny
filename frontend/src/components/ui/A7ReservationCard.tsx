9051df077cebcbb2e34826ce4af9f196e075fec4
Darvel <marsonyteam@gmail.com>
2025-05-27 00:11:02 +0200

    Home page changing top  content

--- frontend/src/components/ui/A7ReservationCard.tsx
+++ frontend/src/components/ui/A7ReservationCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@styles/components/A7ReservationCard.css';
 import buildingSvg from '@assets/img/back_svg.svg';
-import buttonSvg from '@assets/img/button_svg.svg';
+import buttonImg from '@assets/img/button.png';
 
 const A7ReservationCard: React.FC = () => {
   return (
@@ -10,7 +10,7 @@ const A7ReservationCard: React.FC = () => {
 
       <div className="a7-button-wrapper">
         <button className="a7-button" aria-label="Zaloguj się">
-          <img src={buttonSvg} alt="" className="a7-button-image" />
+          <img src={buttonImg} alt="" className="a7-button-image" />
           <span className="a7-button-text">Zaloguj się</span>
         </button>
       </div>
