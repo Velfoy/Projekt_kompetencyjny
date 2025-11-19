6f36fb951ad96352b6e86b9403cfe49455b293e6
Ajeszny <116558609+Ajeszny@users.noreply.github.com>
2025-11-19 13:49:23 +0100

    Update token validator path and working directory

--- backend/backend.AppHost/Program.cs
+++ backend/backend.AppHost/Program.cs
@@ -1,11 +1,20 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var backend = builder.AddProject<Projects.backend>("backend");
-var auth_service = builder.AddExecutable(
-    "tokenvalidator",
-    "D:\\pk\\Projekt_kompetencyjny\\backend\\validate_token\\build\\go_build_validate_token.exe",
-    "../validate_token"
-).WithHttpEndpoint(targetPort:8080, port:8000, name: "gojwt");
+var tokenValidatorPath = "/home/voxel/cas-reservation/Projekt_kompetencyjny/backend/validate_token/build/validate_token";
+var tokenValidatorWorkingDir = "/home/voxel/cas-reservation/Projekt_kompetencyjny/backend/validate_token";
+
+var authService = builder
+    .AddExecutable(
+        "tokenvalidator",
+        tokenValidatorPath,
+        tokenValidatorWorkingDir
+    )
+    .WithHttpEndpoint(
+        name: "gojwt",
+        port: 8000,      // external port
+        targetPort: 8080 // what the executable listens on (if you use it)
+    );
 auth_service.WithReference(backend);
 builder.AddProject<Projects.ConfigDefaults>("configdefaults").WithHttpEndpoint(name: "auth", port: 5167);
 builder.Build().Run();
