f213353afce12deaf4ed6755d0d40b78ec596cb8
Ajeszny <116558609+Ajeszny@users.noreply.github.com>
2025-11-24 15:36:00 +0100

    Zapyatuju dobavil

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -58,7 +58,7 @@ MoXSbUpCmgfd11JhsAgSPHN5eMCicNGCQaIUFbxkfm7p30CVOgJZiDD1NPHnYbug
 r7hnzpLmVbyl4mA4769EhJDREJ639nyO6ymOpuvbriL78CwaWZhWet+pREku0SN8
 O4qQZpG04BxeCYdmFPkFEIrNJbDkjdWOq1Oj8r/B8EOSIhIfhFTTe9S2Gw==
 -----END CERTIFICATE-----`,
-    }
+    },
   build: {
     outDir: '../backend/backend/wwwroot',  // <-- key change
     emptyOutDir: true                      // cleans old files
